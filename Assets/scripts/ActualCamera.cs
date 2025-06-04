f3e5becddd589053b578e52fcd907b072be5822e
Elreyes009 <diegoreyessalvo@gmail.com>
2025-06-04 18:38:14 -0400

    dsddss

--- Assets/scripts/ActualCamera.cs
+++ Assets/scripts/ActualCamera.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 public class ActualCamera : MonoBehaviour
 {
     [SerializeField] GameObject cameraObject;
-    private RayCast ray;
     [SerializeField] List<GameObject> salasLejanas;
     [SerializeField] List<GameObject> salasCercanas;
-    private void Start()
-    {
-        if (ray == null)
-        {
-            ray = FindFirstObjectByType<RayCast>();
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == ray.gameObject)
+        if (collision.CompareTag("Player"))
         {
             cameraObject.SetActive(true);
             foreach (GameObject obj in salasLejanas)
@@ -39,7 +31,7 @@ public class ActualCamera : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject == ray.gameObject)
+        if (collision.CompareTag("Player"))
         {
             cameraObject.SetActive(false);
 
