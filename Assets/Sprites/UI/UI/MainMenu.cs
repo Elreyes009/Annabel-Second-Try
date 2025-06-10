4127ffbd5f06f242494caf9801cf3168a6759d8c
Elreyes009 <diegoreyessalvo@gmail.com>
2025-06-10 15:42:42 -0400

    MainMenu hyper simple y funcional

--- Assets/Sprites/UI/UI/MainMenu.cs
+++ Assets/Sprites/UI/UI/MainMenu.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Nombre de la escena de juego, cámbialo si tu escena tiene otro nombre
+    [SerializeField] private string NameScene = "Juego";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,16 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    // Este método se debe asignar al botón de salir en la UI
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+        print("Saliendo del juego...");
     }
 }
